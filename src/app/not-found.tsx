b7d761d4540a0fd15a8a8bffa834422594bc54c8
Gary Murphy <gardeanmurphy@gmail.com>
2025-08-11 13:57:52 -0500

    Add proper 404 page with status code

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <html>
+      <body>
+        <h1>404 - This page could not be found</h1>
+        <p>Looks like the water’s dried up here 🌊🐶</p>
+      </body>
+    </html>
+  );
+}
